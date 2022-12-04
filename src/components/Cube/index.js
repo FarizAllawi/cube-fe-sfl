391222255cc86bf2b687eef7822d813152e5b44e
fariz allawi <farizallawi24@gmail.com>
2022-12-05 01:28:33 +0700

    Fix Bug

--- src/components/Cube/index.js
+++ src/components/Cube/index.js
@@ -1,46 +1,46 @@
 import Image from 'next/image'
 import React, { useEffect } from 'react'
-import styles from './Cube.module.css'
+import '../../tailwindcss/Cube.module.css'
 import CubeLogo from '/public/images/pictures/cube-logo.png'
 
 export default function Cube() {
   return (
-    <div className={styles.box}>
-        <div className={styles.content}>
-            <div className={styles.singleboxes}>
+    <div className={'box'}>
+        <div className={'content'}>
+            <div className={'singleboxes'}>
 
-                <div className={styles.front}>
-                    <div className={styles.singlebox}>
+                <div className={'front'}>
+                    <div className={'singlebox'}>
                         <Image src={CubeLogo} width={150} height={150} alt="cube-logo" />
                     </div>
                 </div>
 
-                <div className={styles.back}>
-                    <div className={styles.singlebox}>
+                <div className={'back'}>
+                    <div className={'singlebox'}>
                         <Image src={CubeLogo} width={150} height={150} alt="cube-logo" />
                     </div>
                 </div>
 
-                <div className={styles.left}>
-                    <div className={styles.singlebox}>
+                <div className={'left'}>
+                    <div className={'singlebox'}>
                         <Image src={CubeLogo} width={150} height={150} alt="cube-logo" />
                     </div>
                 </div>
 
-                <div className={styles.right}>
-                    <div className={styles.singlebox}>
+                <div className={'right'}>
+                    <div className={'singlebox'}>
                         <Image src={CubeLogo} width={150} height={150} alt="cube-logo" />
                     </div>
                 </div>
 
-                <div className={styles.top}>
-                    <div className={styles.singlebox}>
+                <div className={'top'}>
+                    <div className={'singlebox'}>
                         <Image src={CubeLogo} width={150} height={150} alt="cube-logo" />
                     </div>
                 </div>
 
-                <div className={styles.bottom}>
-                    <div className={styles.singlebox}>
+                <div className={'bottom'}>
+                    <div className={'singlebox'}>
                         <Image src={CubeLogo} width={150} height={150} alt="cube-logo" />
                     </div>
                 </div>
