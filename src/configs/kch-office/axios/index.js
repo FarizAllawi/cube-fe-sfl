2fab02973be05cb813942e76363d8759ca0bcf29
fariz allawi <farizallawi24@gmail.com>
2022-12-20 09:57:14 +0700

    Fix Bug

--- src/configs/kch-office/axios/index.js
+++ src/configs/kch-office/axios/index.js
@@ -4,7 +4,7 @@ import errorHandler from '../../errorHandler'
 const instance = axios.create({
     baseURL: `${process.env.NEXT_PUBLIC_KCH_OFFICE_SERVICE}`,
     headers: {
-        'Access-Control-Allow-Origin' : `${process.env.NEXT_PUBLIC_KCH_OFFICE_SERVICE}`,
+        'Access-Control-Allow-Origin' : `cube-service.sakafarma.com:7022`,
         'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
         'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
         'Accept': 'application/json, text/plain, /',
